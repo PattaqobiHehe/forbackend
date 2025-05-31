c70857f7030c80d8f4e46199ec4dda6dcf8edbc2
Sahil Dhande <118419752+PattaqobiHehe@users.noreply.github.com>
2025-05-31 19:16:22 +0530

    Update otp-verification.js

--- otp-verification.js
+++ otp-verification.js
@@ -24,8 +24,8 @@ const OTPManager = (function() {
   
       try {
         const response = await emailjs.send(
-          window.appConfig.EMAILJS_SERVICE_ID,
-          window.appConfig.EMAILJS_TEMPLATE_ID,
+          window.appConfig.rjCEs-TNXiSnm1aJb,
+          window.appConfig.template_5xqf0yj,
           {
             to_email: email,
             otp_code: generatedOtp,
